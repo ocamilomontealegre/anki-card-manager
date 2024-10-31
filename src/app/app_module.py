331f203fdf50963b1be33c82ab07a2e980bbd37f
ocamilomontealegre <ocamilomontealegre@gmail.com>
2024-10-31 17:01:07 -0500

    chore: update file module

--- src/app/app_module.py
+++ src/app/app_module.py
@@ -2,6 +2,7 @@ from injector import Binder, Module
 from health.health_module import HealthModule
 from modules.file.file_module import FileModule
 
+
 class AppModule(Module):
     def configure(self, binder: Binder) -> None:
         binder.install(HealthModule())
--- src/modules/file/file_module.py
+++ src/modules/file/file_module.py
@@ -1,6 +1,9 @@
 from injector import Module, Binder, singleton
+from modules.file.controllers.file_controller import FileController
+from modules.file.services.file_service import FileService
 
 
 class FileModule(Module):
     def configure(self, binder: Binder) -> None:
-        pass
+        binder.bind(FileController, to=FileController, scope=singleton)
+        binder.bind(FileService, to=FileService, scope=singleton)
