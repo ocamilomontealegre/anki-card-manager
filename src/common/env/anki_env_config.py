204d31c385844e74b0aa595d607fab860e8dd86a
ocamilomontealegre <ocamilomontealegre@gmail.com>
2025-04-07 19:29:43 -0500

    feat: update anki env

--- src/common/env/anki_env_config.py
+++ src/common/env/anki_env_config.py
@@ -3,4 +3,5 @@ from pydantic_settings import BaseSettings
 
 
 class AnkiEnvVariables(BaseSettings):
-    audios: str = Field(description="Path for store the audios")
+    audios: str = Field(description="Path for storing the audios")
+    output: str = Field(description="Path for storing csv output files")
