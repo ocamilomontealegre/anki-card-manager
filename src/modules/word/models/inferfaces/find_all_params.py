c11fd7922554f18380e844fdd6e1e5f83f766bc2
ocamilomontealegre <ocamilomontealegre@gmail.com>
2025-04-17 11:33:18 -0500

    feat(Word): add new query params

--- src/modules/word/models/inferfaces/find_all_params.py
+++ src/modules/word/models/inferfaces/find_all_params.py
@@ -4,6 +4,9 @@ from modules.language.models.enums.language_enum import Language
 
 
 class FindAllParams(BaseModel):
+    limit: Optional[int] = None
+    offset: Optional[int] = None
+    sort: Optional[str] = None
     word: Optional[str] = None
     category: Optional[str] = None
     language: Optional[Language] = None
