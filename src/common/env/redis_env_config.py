993e1a67ef5fc2a3c6bd33a5c0515c4663702b01
ocamilomontealegre <ocamilomontealegre@gmail.com>
2025-05-04 18:52:05 -0500

    feat: support messaque queue env variables

--- src/common/env/redis_env_config.py
+++ src/common/env/redis_env_config.py
@@ -5,4 +5,7 @@ from pydantic_settings import BaseSettings
 class RedisEnvVariables(BaseSettings):
     host: str = Field(default="localhost", description="Redis host")
     port: int = Field(ge=0, default=6379, description="Redis port")
-    db: int = Field(ge=0, default=1, description="Redis database database")
+    db: int = Field(ge=0, default=0, description="Redis database database")
+    mq: int = Field(
+        ge=0, default=1, description="Redis message queue database"
+    )
