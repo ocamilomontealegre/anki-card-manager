6da91eb527cd81e8747ca86f7000a04983fca461
ocamilomontealegre <ocamilomontealegre@gmail.com>
2025-06-20 21:59:48 -0500

    feat(file_utils): add remove_file method

--- src/common/utils/file_utils.py
+++ src/common/utils/file_utils.py
@@ -1,20 +1,33 @@
+from os import remove
 from pathlib import Path
 from common.loggers.logger import AppLogger
+
+logger = AppLogger(label="FileUtils")
 
 
 class FileUtils:
     @staticmethod
     def create_folder(directory_name: str) -> Path:
-        logger = AppLogger()
 
         directory_path = Path(directory_name)
 
         if not FileUtils.check_folder_existence(directory_path):
             directory_path.mkdir(parents=True)
-            logger.info(f"Directory '{directory_name}' created")
+            logger.debug(f"Directory '{directory_name}' created")
 
         return directory_path
 
     @staticmethod
     def check_folder_existence(directory_path: Path) -> bool:
         return directory_path.exists() and directory_path.is_dir()
+
+    @staticmethod
+    def remove_file(file_path: Path) -> None:
+        try:
+            return remove(file_path)
+        except FileNotFoundError:
+            logger.error("File not found")
+        except PermissionError:
+            logger.error("You don't have permission to delete this file.")
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
