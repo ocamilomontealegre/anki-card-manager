9973d82e271575bfb57d24c6433b2b128bfbc398
ocamilomontealegre <ocamilomontealegre@gmail.com>
2025-06-28 20:29:26 -0500

    feat: basically suport verbs

--- src/modules/language/models/interfaces/card_response_interface.py
+++ src/modules/language/models/interfaces/card_response_interface.py
@@ -27,7 +27,10 @@ class CardResponse(BaseModel):
     )
     singular: List[str] = Field(
         default_factory=list,
-        description="A list of singular forms of the word, if applicable, including both feminine and masculine forms. Leave empty if not relevant.",
+        description=(
+            "A list of singular forms of the word, if applicable, including both feminine and masculine forms. Leave empty if not relevant."
+            "If the word is a verb, add the forms following this pattern 'infinitive', 'present', 'present third person', 'past tense', 'past participle', 'ing form'"
+        ),
     )
     synonyms: List[str] = Field(
         default_factory=list,
