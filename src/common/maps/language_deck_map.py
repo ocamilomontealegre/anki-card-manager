7d58535089884b4890c4734288521b46b39ce913
ocamilomontealegre <ocamilomontealegre@gmail.com>
2025-12-29 19:42:45 -0500

    refactor: change decks names

--- src/common/maps/language_deck_map.py
+++ src/common/maps/language_deck_map.py
@@ -1,9 +1,9 @@
 from src.common.enums import Language
 
 language_deck_map: dict[str, str] = {
-    Language.ENGLISH.value: "European Languages::English::Book 10 - Lava",
-    Language.FRENCH.value: "European Languages::Français::Livre 2 - Lave",
+    Language.ENGLISH.value: "Languages::English::Book 10 - Lava",
+    Language.FRENCH.value: "Languages::Français::Livre 2 - Lave",
     Language.GERMAN.value: "",
-    Language.ITALIAN.value: "European Languages::Italiano::Libro 6 - Lava",
+    Language.ITALIAN.value: "Languages::Italiano::Libro 6 - Lava",
     Language.PORTUGUESE.value: "",
 }
