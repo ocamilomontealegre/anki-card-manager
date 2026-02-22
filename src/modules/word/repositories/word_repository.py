de12b21784da930665ce23878c4c8e64126ea657
ocamilomontealegre <ocamilomontealegre@gmail.com>
2026-02-22 18:00:03 -0500

    feat(word): move data source logic to repository

---
+++ src/modules/word/repositories/word_repository.py
@@ -0,0 +1,48 @@
+from word.models.entities.word_entity import Word
+
+from common.database.strategies.database_strategy import DatabaseStrategy
+from modules.word.models.interfaces.find_all_params import ListParams
+
+
+class WordRepository:
+    def __init__(self, db_strategy: DatabaseStrategy):
+        self._session = db_strategy.create_session()
+
+    def _get_filter_query(self, filters: ListParams):
+        query = self._session.query(Word)
+
+        query = query.order_by(Word.created_at.desc())
+
+        if filters.category:
+            query = query.filter(Word.category == filters.category)
+
+        if filters.word:
+            query = query.filter(Word.word.contains(filters.word))
+
+        if filters.language:
+            language = (
+                filters.language.value
+                if hasattr(filters.language, "value")
+                else filters.language
+            )
+            query = query.filter(Word.language == language)
+
+        return query
+
+    def list(self, params: ListParams) -> list[Word]:
+        off_set = params.offset or 0
+        limit = params.limit or 100
+
+        query = self._get_filter_query(params)
+        words = query.offset(off_set).limit(limit).all()
+
+        return words
+
+    def create(self, word: Word) -> Word:
+        self._session.add(word)
+        self._session.commit()
+        self._session.refresh(word)
+
+        return word
+    
+
--- src/modules/word/services/word_service.py
+++ src/modules/word/services/word_service.py
@@ -4,15 +4,13 @@ from uuid import uuid4
 
 from injector import inject
 from pandas import DataFrame
-from sqlalchemy.orm import Session
 
-from common.database.strategies.database_strategy import DatabaseStrategy
 from common.env.env_config import EnvVariables
 from common.loggers.models.abstracts.logger_abstract import Logger
-from common.models import DeleteMany
+from modules.word.repositories.word_repository import WordRepository
 
 from ..models.entities.word_entity import Word
-from ..models.interfaces.find_all_params import FindAllParams
+from ..models.interfaces.find_all_params import ListParams
 from ..transformers.word_transformer import WordTransformer
 
 
@@ -20,77 +18,40 @@ class WordService:
     @inject
     def __init__(
         self,
+        word_repository: WordRepository,
         word_transformer: WordTransformer,
-        db: DatabaseStrategy,
         logger: Logger,
     ) -> None:
         self._file = WordService.__name__
 
-        self._session: Session = db.create_session()
-
+        self._word_repository = word_repository
         self._word_transformer = word_transformer
 
         self._logger = logger
         self._env = EnvVariables.get().anki
 
-    def _get_filter_query(self, filters: FindAllParams):
-        query = self._session.query(Word)
-
-        query = query.order_by(Word.created_at.desc())
-
-        if filters.category:
-            query = query.filter(Word.category == filters.category)
-
-        if filters.word:
-            query = query.filter(Word.word.contains(filters.word))
-
-        if filters.language:
-            language = (
-                filters.language.value
-                if hasattr(filters.language, "value")
-                else filters.language
-            )
-            query = query.filter(Word.language == language)
-
-        return query
-
     def create(self, word: Word) -> Word:
-        self._session.add(word)
-        self._session.commit()
-        self._session.refresh(word)
+        created_word = self._word_repository.create(word)
         self._logger.info(
             f"{Word.__name__}[{word.word}] created successfully.",
             file=self._file,
             method=self.create.__name__,
         )
-        return word
+        return created_word
 
-    def create_many(self, words: list[Word]):
-        self._session.add_all(words)
-        self._session.commit()
-        self._logger.info(
-            f"{len(words)} words created successfully.",
-            file=self._file,
-            method=self.create_many.__name__,
-        )
-
-    def list_paginated(self, filters: FindAllParams) -> tuple[list[Word], int]:
-        off_set = filters.offset or 0
-        limit = filters.limit or 100
-
-        query = self._get_filter_query(filters)
-        words = query.offset(off_set).limit(limit).all()
+    def list(self, filters: ListParams) -> tuple[list[Word], int]:
+        words = self._word_repository.list(filters)
 
         self._logger.info(
             f"{Word.__name__}[{len(words)}] found",
             file=self._file,
-            method=self.list_paginated.__name__,
+            method=self.list.__name__,
         )
 
         return (words, len(words))
 
-    def get_as_csv(self, filters: FindAllParams):
-        result = self._get_filter_query(filters).all()
+    def get_as_csv(self, filters: ListParams):
+        result = self._word_repository.list(filters)
         words = [self._word_transformer.transform(word) for word in result]
         df = DataFrame(words)
 
@@ -106,13 +67,3 @@ class WordService:
             method=self.get_as_csv.__name__,
         )
         return {"status": "OK"}
-
-    def delete_all(self) -> DeleteMany:
-        delete_words = self._session.query(Word).delete()
-        self._session.commit()
-        self._logger.info(
-            f"{Word.__name__}[{delete_words}] deleted successfully",
-            file=self._file,
-            method=self.delete_all.__name__,
-        )
-        return DeleteMany(deleted="OK", total=delete_words)
