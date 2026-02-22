30c911007c2ba0b10234b1578f45b0e7210a3bfc
ocamilomontealegre <ocamilomontealegre@gmail.com>
2026-02-22 17:38:34 -0500

    fix: solve optional types issues

--- src/common/loggers/models/abstracts/logger_abstract.py
+++ src/common/loggers/models/abstracts/logger_abstract.py
@@ -1,26 +1,23 @@
-from typing import Optional
 from abc import ABC, abstractmethod
 
 
 class Logger(ABC):
     @abstractmethod
-    def debug(self, message: str, *, file: str, method: Optional[str] = None) -> None:
+    def debug(self, message: str, *, file: str, method: str | None = None) -> None:
         pass
 
     @abstractmethod
-    def info(self, message: str, *, file: str, method: Optional[str] = None) -> None:
+    def info(self, message: str, *, file: str, method: str | None = None) -> None:
         pass
 
     @abstractmethod
-    def warning(self, message: str, *, file: str, method: Optional[str] = None) -> None:
+    def warning(self, message: str, *, file: str, method: str | None = None) -> None:
         pass
 
     @abstractmethod
-    def error(self, message: str, *, file: str, method: Optional[str] = None) -> None:
+    def error(self, message: str, *, file: str, method: str | None = None) -> None:
         pass
 
     @abstractmethod
-    def critical(
-        self, message: str, *, file: str, method: Optional[str] = None
-    ) -> None:
+    def critical(self, message: str, *, file: str, method: str | None = None) -> None:
         pass
