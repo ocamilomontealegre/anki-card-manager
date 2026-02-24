d792c2fcf538706bcb0449ea0962478506e2536f
ocamilomontealegre <ocamilomontealegre@gmail.com>
2026-02-23 19:32:05 -0500

    feat(app_router): append routers

--- src/app/routers/app_router.py
+++ src/app/routers/app_router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
 from injector import Injector
 
-from health.controllers.health_controller import HealthController
-from modules.anki.controllers.anki_controller import AnkiController
+from health.routers.health_router import HealthRouter
+from modules.anki.routers.anki_router import AnkiRouter
 from modules.language.routers.language_router import LanguageRouter
-from modules.scraper.controllers.scraper_controller import ScraperController
 from modules.task.routers.task_router import TaskRouter
-from modules.upload.controllers.upload_controller import UploadController
+from modules.upload.routers.upload_router import UploadRouter
 from modules.word.routers.word_router import WordRouter
 
 
@@ -21,18 +20,16 @@ class AppRouter:
         return self._router
 
     def _register_routes(self):
-        health_controller = self._injector.get(HealthController)
-        upload_controller = self._injector.get(UploadController)
+        health_router = self._injector.get(HealthRouter)
+        upload_router = self._injector.get(UploadRouter)
         language_router = self._injector.get(LanguageRouter)
         word_router = self._injector.get(WordRouter)
-        scraper_controller = self._injector.get(ScraperController)
-        anki_controller = self._injector.get(AnkiController)
+        anki_router = self._injector.get(AnkiRouter)
         task_router = self._injector.get(TaskRouter)
 
-        self._router.include_router(health_controller.get_router())
-        self._router.include_router(upload_controller.get_router())
+        self._router.include_router(health_router.router)
+        self._router.include_router(upload_router.router)
         self._router.include_router(word_router.router)
-        self._router.include_router(scraper_controller.get_router())
-        self._router.include_router(anki_controller.get_router())
+        self._router.include_router(anki_router.router)
         self._router.include_router(language_router.router)
         self._router.include_router(task_router.router)
