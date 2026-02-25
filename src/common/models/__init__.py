255827c44225a75d7e989e163266e65c00cdebb7
ocamilomontealegre <ocamilomontealegre@gmail.com>
2026-02-24 20:11:50 -0500

    fix: solve typing issues

--- src/common/models/__init__.py
+++ src/common/models/__init__.py
@@ -1,13 +1,11 @@
 from .exception_trace_model import ExceptionTrace
 from .http_response_model import HTTPResponse
-from .task_response_model import TaskResponse
 from .list_paginated_model import ListPaginated
-from .delete_many_model import DeleteMany
+from .task_response_model import TaskResponse
 
 __all__ = [
     "ExceptionTrace",
     "HTTPResponse",
     "TaskResponse",
     "ListPaginated",
-    "DeleteMany",
 ]
--- src/common/models/list_paginated_model.py
+++ src/common/models/list_paginated_model.py
@@ -1,11 +1,12 @@
-from typing import Generic, List, TypeVar
+from typing import Generic, TypeVar
+
 from pydantic import BaseModel
 
 T = TypeVar("T")
 
 
 class ListPaginated(BaseModel, Generic[T]):
-    items: List[T]
+    items: list[T]
     total: int
     page: int
     size: int
