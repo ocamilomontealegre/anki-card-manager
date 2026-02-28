51e28eb91467de5b6b4891845da74af8af85a88c
ocamilomontealegre <ocamilomontealegre@gmail.com>
2026-02-28 15:38:54 -0500

    refactor(upload_service): separate responsabilities

--- src/modules/upload/services/upload_service.py
+++ src/modules/upload/services/upload_service.py
@@ -15,13 +15,21 @@ class UploadService:
     def __init__(self, logger: Logger):
         self.__logger = logger
 
-    async def _write_file_to_disk(self, file_path: Path, file_data: UploadFile) -> Path:
-        with open(file_path, "wb") as file:
-            file.write(await file_data.read())
+    def _build_file_name(self, base_filename: str):
+        timestamp = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")
+        return f"{timestamp}_{base_filename}"
+
+    async def _write_file_to_disk(
+        self,
+        file: UploadFile,
+        destination: Path,
+    ) -> Path:
+        with open(destination, "wb") as buffer:
+            buffer.write(await file.read())
         self.__logger.debug(
-            f"File {file_path} saved successfully",
+            f"File {destination} saved successfully",
         )
-        return file_path
+        return destination
 
     async def save_file(self, file: UploadFile) -> SavedFile:
         if not file.filename:
@@ -32,10 +40,9 @@ class UploadService:
                 f"Content type {file.content_type} currently not supported"
             )
 
-        timestamp = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")
-        file_name = f"{timestamp}_{file.filename}"
+        file_name = self._build_file_name(file.filename)
         file_path = FileUtils.create_folder("uploads") / file_name
-        await self._write_file_to_disk(file_path, file)
+        await self._write_file_to_disk(file, file_path)
         self.__logger.debug(
             f"File {file_name} processed successfully",
         )
